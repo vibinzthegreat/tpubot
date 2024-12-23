6d9f3b02d7573c2c10a5a4b1bef1ffc0d589e974
vibinzthegreat <vibinzthegamer@gmail.com>
2024-12-22 22:20:00 -0600

    actual code

--- bot.py
+++ bot.py
@@ -1,2 +1,46 @@
-print ("ohio")
-print ("skibidi rizz")
+import hikari
+import time
+import lightbulb
+import selenium.common.exceptions
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+gpulink = 'https://www.techpowerup.com/gpu-specs/'
+
+
+driver = webdriver.Chrome()
+driver.get(gpulink)
+
+
+bot = lightbulb.BotApp(intents=hikari.Intents.ALL,  token="MTMyMDU5MTkzNTU1MTA0OTc1OA.GpwvgE.5NyxzuXprGPjJfft9G8FIPyHtdySX3sDilYUu8", prefix='%')
+
+
+@bot.command
+@lightbulb.command('ping', 'says pong')
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def ping(ctx):
+    await ctx.respond('pong!')
+
+elems = driver.find_elements(By.XPATH, "/html/body/div/div[3]/div[1]/form/section/div/fieldset[3]/div/input")
+names = driver.find_elements(By.XPATH, "/html/body/div/div[3]/div[1]/div[2]/table/tbody/tr[1]/td[1]/a")
+
+
+@bot.command
+@lightbulb.option("text", "text to repeat", modifier=lightbulb.OptionModifier.CONSUME_REST)
+@lightbulb.command('gpu', "lists gpu's")
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def gpu(ctx):
+    for elem in elems:
+        elem.send_keys(ctx.options.text)
+    time.sleep(0.5)
+    try:
+        await ctx.respond(driver.find_element(By.XPATH, '/html/body/div/div[3]/div[1]/div[2]/table/tbody/tr[1]/td[1]/a').get_attribute('href'))
+
+    except selenium.common.exceptions.NoSuchElementException:
+        await ctx.respond("No GPU found! Please try again. the proper format is skuname(gt/gtx/rtx) + model number + suffix(xt/super/ti)")
+
+
+    finally:
+        elem.clear()
+
+bot.run()
