613addf67b2224b59320913fa91836acb0ff58a9
vibinz <110127855+vibinzthegreat@users.noreply.github.com>
2024-12-23 23:29:49 -0600

    changed some things

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ driver = webdriver.Chrome()
 driver.get(gpulink)
 
 
-bot = lightbulb.BotApp(intents=hikari.Intents.ALL, token=input("your token here"), prefix='%')
+bot = lightbulb.BotApp(intents=hikari.Intents.ALL, token=input("your token here(change this line to your token if you do not want to save the token every time):   "), prefix='%')
 
 
 @bot.command
